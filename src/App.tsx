52f5d2b0309036891263ba7d6e6ccaa3a721a245
jettison200 <jettison200@gmail.com>
2022-08-15 00:34:24 -0500

    feat: added model and add todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField";
+import { Todo } from "./models/model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          todo: todo,
+          isDone: false,
+        },
+      ]);
+
+      setTodo("");
+    }
+  };
+
   return (
     <div className="app">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <ul>
+        {todos.map((t) => (
+          <li>{t.todo}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => unknown;
 }
 
-export const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+export const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   const submit: (event: React.FormEvent) => unknown = (
     event: React.FormEvent
   ) => {
     event.preventDefault();
   };
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={submit}>
+    <form
+      className="input"
+      onSubmit={(event) => {
+        handleAdd(event);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
         type="input"
         placeholder="enter your task"
         className="input__box"
